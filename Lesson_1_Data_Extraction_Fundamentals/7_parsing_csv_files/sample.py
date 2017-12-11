dfa38d69f2cb6a66122df04ea01b5defff4d38c3
Mengqi Ye <mengqi.ye@yungoal.com>
2017-12-11 15:35:09 +0800

    Add Lessons

--- Lesson_1_Data_Extraction_Fundamentals/7_parsing_csv_files/sample.py
+++ Lesson_1_Data_Extraction_Fundamentals/7_parsing_csv_files/sample.py
@@ -29,6 +29,25 @@ def parse_file(datafile):
     return data
 
 
+def parse_file(datafile):
+    data = []
+    with open(datafile, "r") as f:
+        header = f.readline().split(",")
+        counter = 0
+        for line in f:
+            if counter == 10:
+                break
+            fields = line.split(",")
+            entry = {}
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+
+            data.append(entry)
+            counter += 1
+    print(data)
+    return data
+
+
 def test():
     # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
